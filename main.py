1c6b114eade3d9af097de2abab1b9ae1fd6c318b
Raul <rgomezbe@cisco.com>
2022-02-14 21:42:48 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,156 @@
+from pymongo import MongoClient,errors
+from getpass import getpass
+import pyperclip
+
+
+def connect_mongo(user,pwd):
+
+    host = "mongodb+srv://{}:{}@cluster0.pk2u7.mongodb.net/"
+    arguments = "?tls=true&tlsAllowInvalidCertificates=true"
+    uri_string = host + arguments
+    mongodb_uri = uri_string.format(user, pwd)
+
+    try:
+        client = MongoClient(mongodb_uri)
+        db = client["MyVault"]
+        col = db["connection_test"]
+        test = col.find_one({"name": "test"})["value"]
+        
+        if test:
+            print('Connected to MongoDB!')
+
+        return client
+
+    except errors.OperationFailure as e:
+
+        if e.code == 8000:
+            print(e.details['errmsg'])
+        exit(1)
+
+    except:
+        print('Connection error! Try again later')
+        exit(1)
+        
+
+def get_secret(client):
+    
+    name = input('name: ')
+    db = client["MyVault"]
+    col = db["MyPass"]
+
+    try:
+
+        secret = col.find_one({"name": name})["secret"]
+        pyperclip.copy(secret)
+        print(f'{name} secret saved on clipboard!')
+
+    except TypeError:
+        print(f'Record {name}: not found')
+
+
+def insert_record(client):
+
+    data = dict()
+
+    data['name'] = input('name: ')
+    data['url'] = input('url: ')
+    data['secret'] = getpass('secret: ')
+
+    db = client["MyVault"]
+    col = db["MyPass"]
+    response = col.insert_one(data)
+
+    print('Document saved with id:',response.inserted_id)
+
+
+def list_records(client):
+
+    db = client["MyVault"]
+    col = db["MyPass"]
+    # dir = col.find()
+    dir = col.find({},{ "_id": 0, "secret": 0 })
+
+    for doc in dir:
+        print(doc) 
+
+
+def edit_record(client):
+
+    name = input('name: ')
+    db = client["MyVault"]
+    col = db["MyPass"]
+
+    try:
+
+        col.find_one({"name": name})["secret"]
+        url = input('NEW url: ')
+        secret = getpass('NEW secret: ')
+        filter = { "name": name}
+        newvalues = { "$set": { "secret": secret , "url": url} }
+        col.update_one(filter, newvalues)
+        print(f'{name} updated!')
+
+    except TypeError:
+        print(f'Record {name}: not found')
+
+  
+def delete_record(client):
+
+    name = input('name: ')
+    db = client["MyVault"]
+    col = db["MyPass"]
+
+    try:
+        col.find_one({"name": name})["secret"]
+        filter = { "name": name}
+        col.delete_one(filter)
+        print(f'{name} deleted!')
+
+    except TypeError:
+        print(f'Record {name}: not found')
+    
+
+if __name__ == '__main__':
+
+    user = input('username: ')
+    pwd = getpass('password: ')
+    client = connect_mongo(user,pwd)
+    finish = False
+    menu = '''
+Choose from the following options:
+1. List all records
+2. Retrieve secret
+3. Add new record
+4. Edit existing record
+5. Delete record
+6. Exit
+
+selection : '''
+
+    while not finish:
+
+        option = input(menu)
+
+        if option not in ['1','2','3','4','5','6']:
+
+            print('INVALID OPTION! Please try again')
+
+        else:
+
+            if option == '1':
+                list_records(client)
+
+            elif option == '2':
+                get_secret(client)
+
+            elif option == '3':
+                insert_record(client)
+                
+            elif option == '4':
+                edit_record(client)
+
+            elif option == '5':
+                delete_record(client)
+
+            elif option == '6':
+                finish = True
